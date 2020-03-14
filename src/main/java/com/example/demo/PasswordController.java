fe51f5c4ac17585d3821408d3e0394090ee736db
lenabeliaeva <lena.belia@yandex.ru>
2020-03-14 20:38:55 +0300

    added post method

--- src/main/java/com/example/demo/PasswordController.java
+++ src/main/java/com/example/demo/PasswordController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +10,21 @@ import java.util.Map;
 
 @RestController
 public class PasswordController {
-    Map<String, String> passwords = new HashMap<>();
+    private String response = null;
 
     @GetMapping("/login")
-    public String check_password(String login, String password){
-        passwords.put("lena.belia", "123456aA");
-        if (passwords.containsKey(login)) {
-            if (passwords.get(login).equals(password))
-                return "Пароль верный";
-            else return "Пароль неверный";
-        }
-        else return "Нет такого логина";
+    public void get_info(String login, String password) {
+        String login1 = "lena";
+        String password1 = "123456aA";
+        if (login.equals(login1))
+            if (password.equals(password1))
+                response = "Пароль верный";
+            else response = "Пароль неверный";
+        else response = "Нет такого логина";
+    }
+
+    @PostMapping("/login")
+    public String check_password() {
+        return response;
     }
 }
