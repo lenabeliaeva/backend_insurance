0b47d57e22c26f2b1a47164d634fc66c7669273a
lenabeliaeva <lena.belia@yandex.ru>
2022-05-03 18:46:08 +0300

    spring data added and related stuff

---
+++ src/main/java/com/example/demo/entity/CategoryMock.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+public class CategoryMock {
+    private final int id;
+    private final String name;
+
+    public CategoryMock(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
